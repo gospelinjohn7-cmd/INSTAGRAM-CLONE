95356bea5b8230abfc22f1df976ef3643da8fff2
gjohn-jm <lordjohndevs@gmail.com>
2026-09-22 16:06:19 +0530

    url changed

--- Instaclone/src/components/Register.jsx
+++ Instaclone/src/components/Register.jsx
@@ -22,7 +22,7 @@ function Register() {
 
     const submit=async(e)=>{
         e.preventDefault()
-        axios.post('http://instagramclone-frg6hsahhjhxh6em.eastasia-01.azurewebsites.net/users',register)
+        axios.post('https://instagramclone-frg6hsahhjhxh6em.eastasia-01.azurewebsites.net/users',register)
         .then(()=>console.log("user created"))        
 
     }
